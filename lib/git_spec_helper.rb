81da522cde2237fcd06f726f35a6ca93d5373409
Jeremy Brinkley <jbrinkley@proofpoint.com>
2012-09-18 16:31:23 -0700

    make easier testing, debug testing

--- lib/git_spec_helper.rb
+++ lib/git_spec_helper.rb
@@ -1,6 +1,8 @@
 #!ruby
 # -*- mode: ruby -*-
 
+$debugout = ENV['TEST_DEBUG'] ? '' : '>/dev/null 2>&1'
+
 require 'puppet/marshall/git'
 
 $master_content = <<'EOF'
@@ -59,6 +61,33 @@ def filecontent(file)
     File.open(file, 'r') { |fh| fh.read }
 end
 
+def content(str)
+    <<"EOF"
+class testmod {
+   file {
+      "testmod-indicator":
+          path => "/var/tmp/testmod.indicator",
+          content => "#{str}\n",
+          owner => 'root',
+          group => 'root',
+          mode => 0644;
+   }
+}
+EOF
+end
+
+def makebranch(branch, content=$user_content)
+    Dir.chdir("test/data/testmod") do
+        system "git branch #{branch} #{$debugout}"
+        system "git checkout #{branch} #{$debugout}"
+        File.open("manifests/testmod.pp", 'w') do |fh|
+            fh.print content
+        end
+        system "git add manifests/testmod.pp #{$debugout}"
+        system "git commit -m\"update #{branch} content\" #{$debugout}"
+    end
+end
+
 RSpec.configure do |config|
 
     config.before(:all) do
@@ -69,30 +98,32 @@ RSpec.configure do |config|
             FileUtils.mkdir "test/data"
         end
         Dir.chdir "test/data" do
-            system "git init testmod"
+            system "git init testmod #{$debugout}"
             Dir.chdir "testmod" do
-                system "git config --local user.name \"Tester\""
-                system "git config --local user.email \"test@test.test\""
+                system "git config --local user.name \"Tester\" #{$debugout}"
+                system "git config --local user.email \"test@test.test\" #{$debugout}"
                 FileUtils.mkdir "manifests"
                 File.open("manifests/testmod.pp", 'w') do |fh|
                     fh.print $master_content
                 end
-                system "git add manifests"
-                system "git commit -m\"initial commit\""
-                system "git branch staging"
-                system "git checkout staging"
+                system "git add manifests #{$debugout}"
+                system "git commit -m\"initial commit\" #{$debugout}"
+                system "git branch staging #{$debugout}"
+                system "git checkout staging #{$debugout}"
                 File.open("manifests/testmod.pp", 'w') do |fh|
                     fh.print $staging_content
                 end
-                system "git add manifests/testmod.pp"
-                system "git commit -m\"add staging version\""
-                system "git checkout master"
+                system "git add manifests/testmod.pp #{$debugout}"
+                system "git commit -m\"add staging version\" #{$debugout}"
+                system "git checkout master #{$debugout}"
             end
         end
 
     end
 
     config.after(:all) do
+        # It would be nice to examine exceptions and leave this here for
+        # errors
         FileUtils.rm_r "test/data" if File.exist? "test/data"
     end
 
