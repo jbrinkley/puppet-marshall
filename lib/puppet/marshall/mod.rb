3bdc7c7f682a3bb6591938cfc3e265a7901e0901
Jeremy Brinkley <jbrinkley@proofpoint.com>
2012-09-18 16:11:06 -0700

    add error raising

--- lib/puppet/marshall/mod.rb
+++ lib/puppet/marshall/mod.rb
@@ -10,12 +10,33 @@ module Puppet::Marshall
             self.fullname = fullname
             @settings = settings
             set_from_opthash(opts)
+            # Calculate source if possible - TODO
             if @style.nil?
                 if Puppet::Marshall::Git.is_git? @source
                     @style = 'git'
                 else
                     @style = 'git'
                 end
+            end
+            valid?
+        end
+
+        def source_valid?
+            "no source given for module #{@name}" if @source.nil?
+        end
+
+        def valid?
+            errs = [ ]
+            self.methods.each do |methodname|
+                if /_valid\?$/.match(methodname)
+                    err = self.send(methodname)
+                    errs << err if err
+                end
+            end
+            if errs.size > 0
+                raise Puppet::Marshall::Error.new(
+                                              "errors for module #{@name}: " +
+                                              errs.join("; "))
             end
         end
 
