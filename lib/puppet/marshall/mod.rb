c8a62528e6de573adf8e735e1f4ec5610d0eb720
Jeremy Brinkley <jbrinkley@proofpoint.com>
2012-09-18 15:51:46 -0700

    add calculated style

--- lib/puppet/marshall/mod.rb
+++ lib/puppet/marshall/mod.rb
@@ -4,11 +4,30 @@ module Puppet::Marshall
 
     class Mod
 
-        attr_accessor :name, :fullname, :flavor, :style, :source
+        attr_accessor :name, :flavor, :style, :source
 
-        def initialize(settings, opts)
+        def initialize(settings, fullname, opts)
+            self.fullname = fullname
             @settings = settings
             set_from_opthash(opts)
+            if @style.nil?
+                if Puppet::Marshall::Git.is_git? @source
+                    @style = 'git'
+                else
+                    @style = 'git'
+                end
+            end
+
+        end
+
+        def fullname=(fname)
+            @fullname = fname
+            @name = fname.split('/')[-1]
+            @fullname
+        end
+
+        def fullname
+            @fullname
         end
 
         def marshall
